6cc3a6e3ebbd23efbf8cf001c8879b9971f91015
Alexandre LESUR <alex.lesur.2006@gmail.com>
2025-12-12 16:31:09 +0100

    connexion BDD

---
+++ BDD-Salle-de-Sport/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BDD_Salle_de_Sport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = ConnectToDatabase();
+            connection.Close();
+        }
+        static MySqlConnection ConnectToDatabase()
+        {
+            string login = "";
+            string password = "";
+            Console.Write("Login : ");
+            login = Console.ReadLine();
+            Console.Write("Password : ");
+            password = Console.ReadLine();
+            string connectionString = "server=localhost;user=" + login + ";database=GestionSalleSport;port=3306;password=" + password;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open(); // Ouvre la connexion
+                    Console.WriteLine("Connection to database established successfully.");
+                    InterfaceUtilisateur(connection);
+                    return connection;
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("Error connecting to database: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+        static void ExecuteQuery(MySqlConnection connection, string query)
+        {
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                try
+                {
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString());
+                    }
+                    reader.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("Error executing query: " + ex.Message);
+                }
+            }
+        }
+        static void InterfaceUtilisateur(MySqlConnection connection)
+        {
+
+        }
+    }
+}
