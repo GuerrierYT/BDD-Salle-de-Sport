19bda35a86b1372ce0a35cea6280ea0de6fac024
Roissiope <loris.ligonniere@edu.devinci.fr>
2025-12-15 10:15:31 +0100

    using

--- BDD-Salle-de-Sport/Program.cs
+++ BDD-Salle-de-Sport/Program.cs
@@ -1,5 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BDD_Salle_de_Sport
 {
