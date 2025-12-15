76aeb8bd35949033e0c6daaa34c7d7b57ec02a92
Alexandre LESUR <alex.lesur.2006@gmail.com>
2025-12-15 09:45:07 +0100

    salle

---
+++ BDD-Salle-de-Sport/Salle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_Salle_de_Sport
+{
+    internal class Salle
+    {
+    }
+}
